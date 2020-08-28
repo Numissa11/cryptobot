12ef0c200a4e9976438f00b75f0d5b2467a5f8c7
Aline <aline_sinclaire@hotmail.fr>
2020-08-28 13:16:46 +0200

    api fetched from bitstamp

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const express = require('express') 
+const express = require('express');
 const connection = require('./config')
-
 const app = express()
+const port = process.env.PORT || 3000;
+var fetch = require('node-fetch')
 
 app.use(express.json())
 
-app.listen(3000)
+function ooIfoundData(){
+    var Numissa11='Numissa11'
+    fetch('https://www.bitstamp.net/api/v2/ticker/btceur')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+    }
+ooIfoundData();
+
+
 
 connection.connect((err) => {
       if (err) throw err;
       console.log('database successfully connected')
   })
+
+  app.listen(port, () => console.log(`Listening on port ${port}...`))
