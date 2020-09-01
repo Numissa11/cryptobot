9af464453f09cfc157e57719a3eb70b2efcd4a00
Aline <aline_sinclaire@hotmail.fr>
2020-09-01 14:06:53 +0200

    the 10 last datas from number x are selected fromthe db

--- index.js
+++ index.js
@@ -7,51 +7,77 @@ var cron = require('node-cron');
 
 app.use(express.json())
 
-function ooIfoundData(){
+function ooIfoundData() {
     fetch('https://www.bitstamp.net/api/v2/ticker/btceur')
-  .then(response => response.json())
-  .then(data => {  
-    console.log(data)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
 
-    const timestamp = (data.timestamp)
-    console.log("timestamp :", timestamp)
+            const timestamp = (data.timestamp)
+            console.log("timestamp :", timestamp)
 
-    const open = data.open
-    console.log("open :", open)
+            const open = data.open
+            console.log("open :", open)
 
-    const high = data.high
-    console.log("high :", high)
+            const high = data.high
+            console.log("high :", high)
 
-    const last = data.last
-    console.log("last:", last)
+            const last = data.last
+            console.log("last:", last)
 
-    const low = data.low
-    console.log("low :", low)
-
-    //on doit changer les valeurs pour des numéros dans le knex à mon avis et il faut qu'il reconnaisse que c'est bien ls bonns valeurs
-    const query = `INSERT INTO ohcl_btc_usd (open, high, last, low, timestamp) VALUES ('${open}', '${high}', '${last}', '${low}', '${timestamp}')`
-    console.log('query', query)
-    connection.query(query),
-    (err, res) => {
-        if (err)
-          console.log('error1', err);
-        }
-    })}
-
-  
- // cron.schedule('*/1 * * * *', ooIfoundData());
+            const low = data.low
+            console.log("low :", low)
 
 
- cron.schedule('*/1 * * * *', () => {
+            const query = `INSERT INTO ohcl_btc_usd (open, high, last, low, timestamp) VALUES ('${open}', '${high}', '${last}', '${low}', '${timestamp}')`
+            connection.query(query),
+                (err, res) => {
+                    if (err)
+                        console.log('error1', err);
+                    
+                }            
+        })
+
+        .then(function(req, res, next) {
+            connection.query('SELECT * FROM ohcl_btc_usd limit 7,10', (err, results) => {
+              if (err)
+                console.log(err)
+              else
+                console.log(results);
+              });
+          });
+}
+
+
+
+
+
+   /* connection.query("SELECT * FROM ohcl_btc_usd", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+*/
+
+
+
+
+// cron.schedule('*/1 * * * *', ooIfoundData());
+
+
+cron.schedule('*/1 * * * *', () => {
     ooIfoundData()
     console.log('running a task every minute');
-  });
+});
 
 
 
 connection.connect((err) => {
-      if (err) throw err;
-      console.log('database successfully connected')
-  })
+    if (err) throw err;
+    console.log('database successfully connected')
+})
 
-  app.listen(port, () => console.log(`Listening on port ${port}...`))
+
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}...`))
