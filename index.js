8c4b59b97268ce8cb63685df408035178118c421
Aline <aline_sinclaire@hotmail.fr>
2020-09-18 11:27:21 +0200

    remodeling index file before knex migration

--- index.js
+++ index.js
@@ -35,43 +35,45 @@ let freshData = 0
 async function ooIfoundData() {
     try {
 
-        const fetchedData = await fetch('https://www.bitstamp.net/api/v2/ohlc/btceur?step=60&limit=10') // 'https://www.bitstamp.net/api/v2/ticker/btceur'
+        const fetchedData = await fetch('https://www.bitstamp.net/api/v2/ohlc/btcusd?step=60&limit=10')
 
         const data = await fetchedData.json()
 
         console.log('data', data)
-return
-        const timestamp = data.timestamp
-        // console.log("timestamp :", timestamp)
 
-        const open = data.open
-        // console.log("open :", open)
 
-        const high = data.high
-        // console.log("high :", high)
 
-        const last = data.last
-        // console.log("last:", last)
+        const timestamp = data.data.ohlc[0]['timestamp']
+        console.log("timestamp :", timestamp)
 
-        const low = data.low
-        // console.log("low :", low)
+        const open = data.data.ohlc[0]['open']
+         console.log("open :", open)
+
+        const high = data.data.ohlc[0]['high']
+         console.log("high :", high)
+
+        const close = data.data.ohlc[0]['close']
+         console.log("close:", close)
+
+        const low = data.data.ohlc[0]['low']
+         console.log("low :", low)
 
         // INSERT fetched data into DB
-        const query = `INSERT INTO ohcl_btc_usd (open, high, last, low, timestamp) VALUES ('${open}', '${high}', '${last}', '${low}', '${timestamp}')`
+        const query = `INSERT INTO ohcl_macd_bband (open, high, close, low, timestamp) VALUES ('${open}', '${high}', '${close}', '${low}', '${timestamp}')`
         const insert = await db.queryAsync(query)
-
+/*
         // COUNT total id in the DB
-        const totalLineCount = await db.queryAsync('SELECT COUNT (*) FROM ohcl_btc_usd')
+        const totalLineCount = await db.queryAsync('SELECT COUNT (*) FROM ohcl_macd_bband')
 
         const string = JSON.stringify(totalLineCount);
         const json = JSON.parse(string);
         const numberTotal = json[0]['COUNT (*)']
-        const numberTen = numberTotal - 26;
+        const numberReturned = numberTotal - 26;
         const numberLimit = 26;
 
 
-        // SELECT last 10 from
-        query2 = `SELECT * from ohcl_btc_usd limit ${numberTen}, ${numberLimit}`
+        // SELECT last x number from Table
+        query2 = `SELECT * from ohcl_btc_usd limit ${numberReturned}, ${numberLimit}`
 
         const limit = await db.queryAsync(query2)
 
@@ -81,26 +83,24 @@ return
 
         const openArray = dataJson.map((elem) => elem.open)
         const highArray = dataJson.map((elem) => elem.high)
-        const lastArray = dataJson.map((elem) => elem.last)
+        const closeArray = dataJson.map((elem) => elem.close)
         const lowArray = dataJson.map((elem) => elem.low)
         const timeArray = dataJson.map((elem) => elem.timestamp)
 
-        // console.log(results)
-        // console.log('totalLineCount', totalLineCount)
-        // console.log('limit', limit)
-        // console.log('count: ', numberTotal)
-        // console.log('count -10:', numberTen)
-        // console.log('data Json', dataJson)
-        // console.log('open Array', openArray)
-        // console.log('high Array', highArray)
-        // console.log('last Array', lastArray)
-        // console.log('low Array', lowArray)
-        // console.log('time Array', timeArray)
-        // console.log('open Array length', openArray.length)
+         console.log(results)
+         console.log('totalLineCount', totalLineCount)
+         console.log('limit', limit)
+         console.log('count: ', numberTotal)
+         console.log('count -10:', numberTen)
+         console.log('data Json', dataJson)
+         console.log('open Array', openArray)
+         console.log('high Array', highArray)
+         console.log('last Array', lastArray)
+         console.log('low Array', lowArray)
+         console.log('time Array', timeArray)
+         console.log('open Array length', openArray.length)
 
-      //Functions that take multiple inputs, options, or outputs use arrays.
-//Call Stochastic Oscillator, taking 3 inputs, 3 options, and 2 outputs.
-
+ 
         // query3 = `SELECT * from settings where indicatior_name = 'macd'`
 
         // const indicatorSettings = await db.queryAsync(query3)
@@ -108,7 +108,7 @@ return
         const macd = await tulind.indicators.macd.indicator([openArray], [12, 26, 9])
 
         console.log('macd', macd)
-
+*/
 
     } catch (error) {
         console.log(error);
@@ -120,12 +120,12 @@ return
 
 // console.log('indicators', tulind.indicators);
           
-
+/*
 app.get('/tradingData', (req, res) => {
 
     res.json(freshData)
 })
-
+*/
 
 cron.schedule('*/1 * * * *', async () => {
     await ooIfoundData()
