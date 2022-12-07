0644066a9215de8fe7b99be90fb600579d0cc8e1
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2022-12-07 17:13:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const  connection = mysql.createConnection({
 host :  'localhost', // adresse du serveur
 port : 3307,
 user :  'user', // le nom d'utilisateur
-password :  '', // mon mot de passe habituel
+password :  'monmotdepassehabituel', // mon mot de passe habituel
 database :  'cryptodb', // le nom de la base de données
 });
 module.exports = connection
